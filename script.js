8ebcd05a6054e5a3ebcc176d0343d35cd3fa4676
hotsoup42 <jose22gw@gmail.com>
2023-11-09 15:40:54 -0800

    formatted document and changed cheveron button margin

--- script.js
+++ script.js
@@ -1,59 +1,50 @@
 const currentDate = document.querySelector(".current-date");
 const days = document.querySelector(".days")
-
-const icons  = document.querySelectorAll(".icons span")
+const icons = document.querySelectorAll(".icons span")
 
 let date = new Date(),
-currentYear = date.getFullYear(),
-currentMonth = date.getMonth();
-
+    currentYear = date.getFullYear(),
+    currentMonth = date.getMonth();
 
 const months = ["January", "Febuary", "March", "April", "May", "June", "July",
     "August", "September", "October", "November", "December"];
 
 const render = () => {
     let liElement = ""
-  
     let firstDayofMonth = new Date(currentYear, currentMonth, 1).getDay() //first Day  of the Month
-    lastDateofMonth = new Date(currentYear, currentMonth +1,0).getDate() //last Date of the Month
+    lastDateofMonth = new Date(currentYear, currentMonth + 1, 0).getDate() //last Date of the Month
     lastDayofMonth = new Date(currentYear, currentMonth).getDay() //last Date of the Month
     lastDateofLastMonth = new Date(currentYear, currentMonth, 0).getDate() //last Date of the Last Month
 
-
-    for (let i = firstDayofMonth; i > 0; i--){   
+    for (let i = firstDayofMonth; i > 0; i--) {
         liElement += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
-
     }
-        
-    for (let i= 1; i <= lastDateofMonth; i++){   
+
+    for (let i = 1; i <= lastDateofMonth; i++) {
         let isToday = i === date.getDate() && currentMonth === new Date().getMonth()
-        && currentYear === new Date().getFullYear() ? "active" : "";
-   
+            && currentYear === new Date().getFullYear() ? "active" : "";
         liElement += `<li class="${isToday}"> ${i} </li>`
-        
     }
 
-    for (let i = lastDayofMonth; i < 11; i++){   
-   
+    for (let i = lastDayofMonth; i < 11; i++) {
         liElement += `<li class="inactive">${i - lastDayofMonth + 1}</li>`
-
     }
     currentDate.innerText = ` ${months[currentMonth]} ${currentYear}`
-    days.innerHTML=liElement
+    days.innerHTML = liElement
 }
 render();
 
- //next/prev month buttons
+//next/prev month buttons
 icons.forEach(icon => {
     icon.addEventListener("click", () => {
-       currentMonth = icon.id === "prev" ? currentMonth - 1 : currentMonth + 1;
-        if(currentMonth < 0 || currentMonth > 11) {
+        currentMonth = icon.id === "prev" ? currentMonth - 1 : currentMonth + 1;
+        if (currentMonth < 0 || currentMonth > 11) {
             date = new Date(currentYear, currentMonth);
             currentYear = date.getFullYear();
             currentMonth = date.getMonth();
-        } else { 
+        } else {
             date = new Date();
         }
-       render();
-    } )
+        render();
+    })
 })
