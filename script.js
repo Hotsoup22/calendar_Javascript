b72d486fa299e054a89d095d8eae2e507e9bfe7c
hotsoup42 <jose22gw@gmail.com>
2023-11-09 15:35:10 -0800

    script now calls for new date() gets days for the Calendar. Last months days and next month days are shown and have active/inactive classes. changes made to index.html in ref to those changes.

--- script.js
+++ script.js
@@ -1,25 +1,59 @@
 const currentDate = document.querySelector(".current-date");
 const days = document.querySelector(".days")
+
+const icons  = document.querySelectorAll(".icons span")
+
 let date = new Date(),
 currentYear = date.getFullYear(),
 currentMonth = date.getMonth();
+
 
 const months = ["January", "Febuary", "March", "April", "May", "June", "July",
     "August", "September", "October", "November", "December"];
 
 const render = () => {
-    let lastDateofMonth = new Date(currentYear, currentMonth +1,0).getDate() //last Date of the Month
-    
+    let liElement = ""
+  
+    let firstDayofMonth = new Date(currentYear, currentMonth, 1).getDay() //first Day  of the Month
+    lastDateofMonth = new Date(currentYear, currentMonth +1,0).getDate() //last Date of the Month
+    lastDayofMonth = new Date(currentYear, currentMonth).getDay() //last Date of the Month
+    lastDateofLastMonth = new Date(currentYear, currentMonth, 0).getDate() //last Date of the Last Month
 
-    for (let i= 1; i <= lastDateofMonth; i++){
-        let liElement = document.createElement("li") ;
-        liElement.textContent = `${i}`
-        days.appendChild(liElement)
-        console.log(i)
+
+    for (let i = firstDayofMonth; i > 0; i--){   
+        liElement += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
+
+    }
+        
+    for (let i= 1; i <= lastDateofMonth; i++){   
+        let isToday = i === date.getDate() && currentMonth === new Date().getMonth()
+        && currentYear === new Date().getFullYear() ? "active" : "";
+   
+        liElement += `<li class="${isToday}"> ${i} </li>`
+        
     }
 
-    currentDate.textContent = ` ${months[currentMonth]}, ${currentYear}`
-      
-        
+    for (let i = lastDayofMonth; i < 11; i++){   
+   
+        liElement += `<li class="inactive">${i - lastDayofMonth + 1}</li>`
+
+    }
+    currentDate.innerText = ` ${months[currentMonth]} ${currentYear}`
+    days.innerHTML=liElement
 }
 render();
+
+ //next/prev month buttons
+icons.forEach(icon => {
+    icon.addEventListener("click", () => {
+       currentMonth = icon.id === "prev" ? currentMonth - 1 : currentMonth + 1;
+        if(currentMonth < 0 || currentMonth > 11) {
+            date = new Date(currentYear, currentMonth);
+            currentYear = date.getFullYear();
+            currentMonth = date.getMonth();
+        } else { 
+            date = new Date();
+        }
+       render();
+    } )
+})
